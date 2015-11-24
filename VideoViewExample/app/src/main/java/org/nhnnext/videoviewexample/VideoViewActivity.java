afacac0a72346d4e9b9e737de79143587fef66c6
luvgaram <garam@nhnnext.org>
2015-11-25 03:52:44 +0900

    GDG Korea meetup: android MediaPlayer and VideoView

---
+++ VideoViewExample/app/src/main/java/org/nhnnext/videoviewexample/VideoViewActivity.java
@@ -0,0 +1,41 @@
+package org.nhnnext.videoviewexample;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import java.net.MalformedURLException;
+
+
+public class VideoViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_videoview);
+
+        VideoView videoView = (VideoView)findViewById(R.id.videoView);
+
+        // internal URI
+        Uri uri = Uri.parse("android.resource://" + getPackageName() + "/raw/too_cute");
+
+        // external URL
+//        String path = "http://techslides.com/demos/sample-videos/small.mp4";
+//        Uri uri= Uri.parse(path);
+        videoView.setVideoURI(uri);
+
+
+        final MediaController mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+
+        videoView.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mediaController.show(0);
+            }
+        }, 100);
+
+    }
+}
